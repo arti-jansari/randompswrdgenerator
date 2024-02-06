12f0453bdf2a40f734bf376239d26739ccfe2046
arti jansari <artijansari@gmail.com>
2024-02-06 14:45:08 +0530

    add cHNGS

--- src/App.js
+++ src/App.js
@@ -94,12 +94,7 @@ return (
   <div style={mainContainer}>
 <div style={containerStyle}>     
       <h1 style={{ textAlign: "center" ,color:"cadetblue"}}>Random Password Generator</h1>
-      <form>
-  <div class="form-group">
-  <label style={labelStyle}>Password Length:</label>
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-  </div>
-  </form>
+    
       <div style={inputContainerStyle}>
           <label style={labelStyle}>Password Length:</label>
           <input
